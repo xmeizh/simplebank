b5612807847a7dffc2fcece40a87e63ee2125d14
Xuemei Zhang <xuemei.zhang@jambit.com>
2024-05-14 16:43:12 +0200

    feat: add HTTP API with Gin

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/xmeizh/simplebank/db/postgresql"
+)
+
+// Server serves HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts", server.listAccounts)
+	router.GET("/accounts/:id", server.getAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
+
+	server.router = router
+	return server
+}
+
+// Start runs the HTTP server on a specific address
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
