e83a3fcc0868e501eb33b1dac0afd0baba23f1bd
Xuemei Zhang <xuemei.zhang@jambit.com>
2024-05-16 10:45:05 +0200

    fix: validate params using validator

--- api/server.go
+++ api/server.go
@@ -2,6 +2,8 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	db "github.com/xmeizh/simplebank/db/postgresql"
 )
 
@@ -15,10 +17,15 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts", server.listAccounts)
 	router.GET("/accounts/:id", server.getAccount)
 	router.DELETE("/accounts/:id", server.deleteAccount)
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
