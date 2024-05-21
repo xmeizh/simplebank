cffe312d5d77dc468f26ea308a55e90a7fe3c589
Xuemei Zhang <xuemei.zhang@jambit.com>
2024-05-21 18:05:55 +0200

    feat: create & verify JWT & Paseto token

---
+++ token/maker.go
@@ -0,0 +1,17 @@
+package token
+
+import (
+	"errors"
+	"time"
+)
+
+var ErrInvalidToken = errors.New("token is invalid")
+var ErrTokenExpired = errors.New("token is expired")
+
+// Maker is an interface for managing tokens
+type Maker interface {
+	CreateToken(username string, duration time.Duration) (string, error)
+
+	// VerifyToken checks if the token is valid or not
+	VerifyToken(token string) (*Payload, error)
+}
