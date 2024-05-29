e97dcb8cd46757c423f0135442e7918e30664308
Xuemei Zhang <xuemeizh@outlook.com>
2024-05-29 18:38:47 +0200

    chore: deploy application to AWS EKS

---
+++ eks/install.sh
@@ -0,0 +1,2 @@
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/aws/deploy.yaml
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.14.5/cert-manager.yaml
