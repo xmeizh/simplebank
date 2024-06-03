d6d78259df8891d4b0fa37b03614fa5bc0d3e884
xmeizh <xuemeizh@outlook.com>
2024-06-03 14:35:02 +0200

    feat: refresh token (#9)

--- token/maker.go
+++ token/maker.go
@@ -10,7 +10,7 @@ var ErrTokenExpired = errors.New("token is expired")
 
 // Maker is an interface for managing tokens
 type Maker interface {
-	CreateToken(username string, duration time.Duration) (string, error)
+	CreateToken(username string, duration time.Duration) (string, *Payload, error)
 
 	// VerifyToken checks if the token is valid or not
 	VerifyToken(token string) (*Payload, error)
