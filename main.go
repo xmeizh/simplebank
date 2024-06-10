f0ef61e435a2e735ad611649ff85dc6f93b2d529
Xuemei Zhang <xuemei.zhang@jambit.com>
2024-06-10 17:27:56 +0200

    feat: add gRPC gateway & swagger API doc

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq" // Without this package our server cannot connect to the db
 	"github.com/xmeizh/simplebank/api"
 	db "github.com/xmeizh/simplebank/db/postgresql"
@@ -14,6 +17,7 @@ import (
 	"github.com/xmeizh/simplebank/util"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func main() {
@@ -29,6 +33,7 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
+	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
 }
 
@@ -62,5 +67,49 @@ func runGrpcServer(config util.Config, store db.Store) {
 	err = grpcServer.Serve(lis)
 	if err != nil {
 		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGatewayServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	jsonOption := runtime.WithMarshalerOption(runtime.MIMEWildcard, &runtime.JSONPb{
+		MarshalOptions: protojson.MarshalOptions{
+			UseProtoNames: true,
+		},
+		UnmarshalOptions: protojson.UnmarshalOptions{
+			DiscardUnknown: true,
+		},
+	})
+
+	gwmux := runtime.NewServeMux(jsonOption)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	err = pb.RegisterSimpleBankHandlerServer(ctx, gwmux, server)
+	if err != nil {
+		log.Fatal("cannot register handler server")
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/", gwmux)
+
+	fs := http.FileServer(http.Dir("./doc/swagger"))
+	mux.Handle("/swagger/", http.StripPrefix("/swagger/", fs))
+
+	lis, err := net.Listen("tcp", config.HTTPServerAddress)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	log.Printf("start HTTP gateway server at %s", lis.Addr().String())
+	err = http.Serve(lis, mux)
+	if err != nil {
+		log.Fatal("cannot start HTTP gateway server")
 	}
 }
