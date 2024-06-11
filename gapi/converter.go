199e30cf1e80387860f5cc6e663a3cca2bbf1811
Xuemei Zhang <xuemei.zhang@jambit.com>
2024-06-11 17:04:52 +0200

    chore: optimize gRPC validator

---
+++ gapi/converter.go
@@ -0,0 +1,17 @@
+package gapi
+
+import (
+	db "github.com/xmeizh/simplebank/db/postgresql"
+	"github.com/xmeizh/simplebank/pb"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+func convertUser(user db.User) *pb.User {
+	return &pb.User{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Username,
+		PasswordChangedAt: timestamppb.New(user.PasswordChangedAt),
+		CreatedAt:         timestamppb.New(user.CreatedAt),
+	}
+}
